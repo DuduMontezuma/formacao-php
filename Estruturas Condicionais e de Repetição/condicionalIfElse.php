9137c2583a4322ece49b848466afcbf34cd31a93
DuduMontezuma <carlos15montezuma@gmail.com>
2023-11-07 20:44:52 -0300

    Terminei a aula de ELSE

---
+++ Estruturas Condicionais e de Repetição/condicionalIfElse.php
@@ -0,0 +1,15 @@
+<?php
+
+    $nota = 3;
+
+    // if ($nota >= 7) {
+    //    echo "APROVADO";
+    // } else{
+    //     echo "REPROVADO";
+    // }
+
+
+
+    echo $nota >= 7 ? "APROVADO" : "REPROVADO";
+
+?>
