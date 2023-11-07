4c2addf0f5c9d165f409314499f8e7419cfbc1f8
DuduMontezuma <carlos15montezuma@gmail.com>
2023-11-07 17:34:21 -0300

    Terminei a aula de IF

---
+++ Estruturas Condicionais e de Repetição/condicionalif.php
@@ -0,0 +1,10 @@
+<?php
+
+    $variavel1 = true;
+    $variavel2 = false;
+    $variavel3 = true;
+
+    if ($variavel1 == $variavel2 && 10 < 5) {
+        echo "Variáveis iguais e 10 é menor que 51";
+    }
+?>
