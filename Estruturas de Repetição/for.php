c28d4ec6f676a172c29a296272f5c7a40752c04e
DuduMontezuma <carlos15montezuma@gmail.com>
2023-11-08 10:34:43 -0300

    Termieni a aula de FOR

---
+++ Estruturas de Repetição/for.php
@@ -0,0 +1,9 @@
+<?php
+
+   $arrayNumeros = [3, 5, 6, 1, 2];
+    $qtd_elementos_array = count($arrayNumeros);
+
+    for ($i = 0; $i < $qtd_elementos_array; $i++){
+        print_r($arrayNumeros[$i] . "\n");
+    }
+?>
